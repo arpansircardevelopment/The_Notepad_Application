95c7f07a6ca11925f8316ea27fbc7dac915b22c5
Arpan Sircar <https://github.com/arpansircardevelopment/Kotlin-Currency-Conversion-Application-Using-MVVM->
2020-09-14 21:38:30 +0530

    Created the query method in the DatabaseRepository.kt class

--- app/src/main/java/com/kotlin/thenotepadapplication/model/NotepadContract.kt
+++ app/src/main/java/com/kotlin/thenotepadapplication/model/NotepadContract.kt
@@ -1,8 +1,11 @@
 package com.kotlin.thenotepadapplication.model
 
+import android.provider.BaseColumns
+
 object NotepadContract {
 
     const val SQLITE_CREATE_ENTRY = "CREATE TABLE ${NotepadEntry.TABLE_NAME} (" +
+            "${BaseColumns._ID} PRIMARY KEY" +
             "${NotepadEntry.COLUMN_TITLE} TEXT, " +
             "${NotepadEntry.COLUMN_SUBTITLE} TEXT, " +
             "${NotepadEntry.COLUMN_DATE} TEXT)"
@@ -10,7 +13,7 @@ object NotepadContract {
     const val DATABASE_NAME: String = "notepad.db"
     const val DATABASE_VERSION: Int = 1
 
-    object NotepadEntry {
+    object NotepadEntry : BaseColumns {
         const val TABLE_NAME = "entries"
         const val COLUMN_TITLE = "title"
         const val COLUMN_SUBTITLE = "subtitle"
--- app/src/main/java/com/kotlin/thenotepadapplication/repository/DatabaseRepository.kt
+++ app/src/main/java/com/kotlin/thenotepadapplication/repository/DatabaseRepository.kt
@@ -2,30 +2,68 @@ package com.kotlin.thenotepadapplication.repository
 
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
 import com.kotlin.thenotepadapplication.model.NotepadContract
 import com.kotlin.thenotepadapplication.model.NotepadEntryDatabaseHelper
+import com.kotlin.thenotepadapplication.model.NotepadEntryPOJO
 
 class DatabaseRepository(context: Context) {
     private val notepadEntryDatabaseHelper: NotepadEntryDatabaseHelper =
         NotepadEntryDatabaseHelper(context)
 
     fun insertMethod(title: String, subtitle: String, date: String): Long {
-        val sqliteDatabaseHelper: SQLiteDatabase = notepadEntryDatabaseHelper.writableDatabase
+        val sqliteDatabase: SQLiteDatabase = notepadEntryDatabaseHelper.writableDatabase
         val insertValues: ContentValues = ContentValues().apply {
             put(NotepadContract.NotepadEntry.COLUMN_TITLE, title)
             put(NotepadContract.NotepadEntry.COLUMN_SUBTITLE, subtitle)
             put(NotepadContract.NotepadEntry.COLUMN_DATE, date)
         }
 
-        return sqliteDatabaseHelper.insert(
+        return sqliteDatabase.insert(
             NotepadContract.NotepadEntry.TABLE_NAME,
             null,
             insertValues
         )
     }
 
-    fun queryMethod(){
+    fun queryMethod(): ArrayList<NotepadEntryPOJO> {
+        val sqLiteDatabase: SQLiteDatabase = notepadEntryDatabaseHelper.readableDatabase
+        val sortOrder = "${BaseColumns._ID} DESC"
 
+        val projection = arrayOf(
+            NotepadContract.NotepadEntry.TABLE_NAME,
+            NotepadContract.NotepadEntry.COLUMN_TITLE,
+            NotepadContract.NotepadEntry.COLUMN_DATE
+        )
+
+        val cursor: Cursor = sqLiteDatabase.query(
+            NotepadContract.NotepadEntry.TABLE_NAME,
+            projection,
+            null,
+            null,
+            null,
+            null,
+            sortOrder
+        )
+
+        return cursorParseMethod(cursor)
+    }
+
+    private fun cursorParseMethod(cursor: Cursor): ArrayList<NotepadEntryPOJO> {
+        val databaseItems: ArrayList<NotepadEntryPOJO> = ArrayList()
+
+        with(cursor) {
+            while (cursor.moveToNext()) {
+                val titleItem = getString(getColumnIndex(NotepadContract.NotepadEntry.COLUMN_TITLE))
+                val subtitleItem =
+                    getString(getColumnIndex(NotepadContract.NotepadEntry.COLUMN_SUBTITLE))
+                val dateItem = getString(getColumnIndex(NotepadContract.NotepadEntry.COLUMN_DATE))
+                databaseItems.add(NotepadEntryPOJO(titleItem, subtitleItem, dateItem))
+            }
+        }
+
+        return databaseItems
     }
 }
